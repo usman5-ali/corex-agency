d01b33f0fea039b0c11b1a332e13b3e64af13f22
harisarif <harif3496@gmail.com>
2025-07-17 23:04:43 +0500

    scroll fixed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Router from './components/Routes/Router';
-
+import ScrollToTop from './components/ScrollTop'
 
 
 
 function App() {
   return (
     <div className="App">
+       <ScrollToTop />
  <Router />
     </div>
   );
